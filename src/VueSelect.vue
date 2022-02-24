159a2593a46bc888c611e7a0ec5e61f06398a0ba
Salma EL HAJRAOUI <salma.hajrawi@gmail.com>
2022-02-24 17:31:45 +0100

    add select element

---
+++ src/VueSelect.vue
@@ -0,0 +1,56 @@
+<template>
+  <label
+    v-show="label"
+    :class="labelClass"
+  >
+    {{ label }}
+  </label>
+  <select
+    v-bind="{
+      ...$attrs,
+      onChange: ($event) => {$emit('update:modelValue', $event.target.value)}
+    }"
+    :value="modelValue"
+    :class="seletClass"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >
+      {{ option }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'VueSelect',
+
+  emits: ['update:modelValue'],
+
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    labelClass: {
+      type: String,
+      default: ''
+    },
+    seletClass: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number, Boolean],
+      default: ''
+    },
+    options: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
