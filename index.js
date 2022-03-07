7099821ad606877d9789b8e5472a5a3ba156ac12
Salma EL HAJRAOUI <salma.hajrawi@gmail.com>
2022-03-07 17:10:12 +0100

    plugin install in progress...

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import VueInput from './src/VueInput.vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+  './src',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
 
 export default {
   install(Vue, options) {
-    Vue.component('VueInput', VueInput)
-  }
+    requireComponent.keys().forEach(fileName => {
+      const componentConfig = requireComponent(fileName)
+
+      const componentName = upperFirst(
+        camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+      )
+
+      Vue.component(componentName, componentConfig.default || componentConfig)
+    })
 }
