5d027cd7c76212ed4ab4508608d1e1426ce8b484
Salma EL HAJRAOUI <salma.hajrawi@gmail.com>
2022-03-08 14:33:04 +0100

    fix import script

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ import camelCase from 'lodash/camelCase'
 
 const requireComponent = require.context(
   './src',
-  false,
-  /Base[A-Z]\w+\.(vue|js)$/
+  true,
+  /\.vue$/
 )
 
 export default {
   install(Vue, options) {
     requireComponent.keys().forEach(fileName => {
       const componentConfig = requireComponent(fileName)
-
       const componentName = upperFirst(
         camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
       )
-
       Vue.component(componentName, componentConfig.default || componentConfig)
     })
   }
 }
-
